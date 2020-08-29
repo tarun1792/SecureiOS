52648a9ecda16d8ed011f98139b64f6906d98d6c
tarun-lucideus <tarun.k@lucideustech.com>
2020-08-29 21:17:50 +0530

    Initial Commit

---
+++ SecureiOS/SecureiOS.h
@@ -0,0 +1,19 @@
+//
+//  SecureiOS.h
+//  SecureiOS
+//
+//  Created by Tarun Kaushik on 29/08/20.
+//  Copyright © 2020 Tarun Kaushik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecureiOS.
+FOUNDATION_EXPORT double SecureiOSVersionNumber;
+
+//! Project version string for SecureiOS.
+FOUNDATION_EXPORT const unsigned char SecureiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureiOS/PublicHeader.h>
+
+
